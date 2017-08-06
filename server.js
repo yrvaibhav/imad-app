9c225072f61f4d25a2925a25920ccf47e7e8dc54
yrvaibhav <30660178+yrvaibhav@users.noreply.github.com>
2017-08-06 20:08:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 
 var articalone={
-    titel: 'Artical one | Vaibhav YR',
+    title: 'Artical one | Vaibhav YR',
     heading: 'Artical one',
     date: 'August 3 2017',
     content: ` <p>
@@ -17,57 +17,50 @@ var articalone={
             I'm happy in doing this.
         </p>
         <p>
+            This is wonder full course. 
         </p>
 };
-var htmlTemplate =`
-<html>
-<head>
-    <title>
-        ${title}
-    </title>
-    <link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
-    <div>
-        <a href="/">HOME</a>
-    </div>
-    <hr/>
-    <h3>
-        ${heading}
-    </h3>
-    <div>
-        ${date}
-    </div>
-    <div>
-       ${content}
-    </div>
-    </div>
-</body>
-</html>
+function createTemplate (data) {
+    var title = data.title;
+    var data = data.data;
+    var heading = data.heading;
+    var content = data.content;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-`;
-
+    var htmlTemplate =`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/artical-1',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+    res.send(createTemplate(articalOne));
 });
 
 app.get('/artical-2',function(req,res){
