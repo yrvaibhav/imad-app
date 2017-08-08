4859fa2ac672fd09ea26cda9f0b65957d32e3f3c
yrvaibhav <30660178+yrvaibhav@users.noreply.github.com>
2017-08-08 23:43:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML ='New Value';
+//Move the image
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+};
