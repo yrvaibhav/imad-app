634ce10b57b511d99cdf99231ad2dd10a0d1f9c8
yrvaibhav <30660178+yrvaibhav@users.noreply.github.com>
2017-08-08 23:50:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element = document.getElementById('main-text');
 element.innerHTML ='New Value';
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight,100);
+    
 };
