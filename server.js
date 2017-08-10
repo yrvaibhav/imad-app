5e22c4ef0e3541a36076bcf1278cb7dc60c0bfdc
yrvaibhav <30660178+yrvaibhav@users.noreply.github.com>
2017-08-10 22:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articalName',function(req,res){
     //articalName == artical-1
     //articales[articalName] == {} content object for artical one
